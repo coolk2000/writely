781b65f12bcefbd01dd58dc0dea57bbd7fe522b0
Jacob Koenen <koenenjacob@hotmail.com>
2015-02-11 22:52:29 -0500

    CSS, footer

--- index.php
+++ index.php
@@ -7,15 +7,16 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="modules/bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href="footer.css" rel="stylesheet">
 	</head>
 	<body>
-		<div class="container">
-			<div class="jumbotron vertical-center">
-				<h1>index</h1>
-				<small>
-					yay!
-				</small>
+		<footer class="footer">
+			<div class="container">
+				<div class="text-muted">
+					<span class="glyphicon glyphicon-copyright-mark" aria-hidden="true"></span>
+					2015 Jake Koenen
+				</div>
 			</div>
-		</div>
+		</footer>
 	</body>
 </html>
