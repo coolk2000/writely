179475b7786ccbd1fde1abf0c75b082eaa7a5430
NigelNoscopes <1koenenjacob@gmail.com>
2015-02-12 17:56:34 -0500

    Started working on database and connection

---
+++ includes/db_config.php
@@ -0,0 +1,11 @@
+<?php
+define("HOST", "localhost");
+define("USER", "writely");
+define("PASSWORD", "Z5wnZ2YaczYdMTP4");
+define("DATABASE", "writely");
+
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "user");
+
+define("SECURE", FALSE);	// dev only
+?>
