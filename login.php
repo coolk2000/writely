8afc61ca86d110f2fc5ce7e876f4f3eb139af6c7
Jacob Koenen <koenenjacob@hotmail.com>
2015-02-12 15:30:24 -0500

    Added index page, put css into own folder

--- login.php
+++ login.php
@@ -8,20 +8,32 @@
 		<link rel="stylesheet" type="text/css" href="modules/bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link href="css/footer.css" rel="stylesheet">
-		<link href="css/index.css" rel="stylesheet">
+		<link href="css/login.css" rel="stylesheet">
 	</head>
 	<body>
 		<div class="container">
 			<div class="centered text-center">
 				<div class="jumbotron">
-					<h1>writely</h1>
-					<form action="login">
+					<form class="form-inline">
+						<div class="form-group">
+							<label class="sr-only" for="inputUsername">Username</label>
+							<input type="username" class="form-control" id="inputUsername" placeholder="Username">
+						</div>
+						<div class="form-group">
+							<label class="sr-only" for="inputPassword">Password</label>
+							<input type="password" class="form-control" id="inputPassword" placeholder="Password">
+						</div>
+						<!-- <div class="checkbox">
+							<label>
+								<input type="checkbox"> Remember me
+							</label>
+						</div> -->
 						<button type="submit" class="btn btn-primary">
-							Login
+							<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
 						</button>
 					</form>
-				</div>
-			</div>
+    			</div>
+    		</div>
 		</div>
 		<footer class="footer">
 			<div class="container">
