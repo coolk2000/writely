d9b8d33dc871b8656685fd77667707c2996b1a78
NigelNoscopes <1koenenjacob@gmail.com>
2015-02-19 08:54:07 -0500

    Oops.

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -31,17 +31,15 @@ if (isset($_POST['username'], $_POST['p'], $_POST['g-recaptcha-response'])) {
 	$stmt = "SELECT id FROM users WHERE username = ? LIMIT 1";
 	$stmt_ready = true;
 
-	if ($stmt_ready == true) {
+	if ($stmt = $db->prepare("SELECT id FROM users WHERE username = ? LIMIT 1")) {
 		$stmt->execute(array($username));
 				if (($stmt->fetchColumn() > 0)) {
 						// TODO: properly handle errors by displaying them nicely to the user
 						$error_msg .= 'A user with this username already exists';
-						$stmt->close();
 				}
 		} else {
 				// TODO: properly handle errors by displaying them nicely to the user
 				$error_msg .= 'Database error';
-				$stmt->close();
 		}
 
 	if (empty($error_msg)) {
